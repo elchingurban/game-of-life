8b95dbc2e3fca504dfea0eea66bad5da145463a8
Elchin Gurbanli <elchingurban@Laptop-de-Elchin.local>
2021-06-13 03:26:52 +0400

    STEP-2: print() function was deleted from Game library

--- step-1/game.h
+++ step-1/game.h
@@ -1,5 +1,3 @@
 void create(int *array[], int rows, int columns);
 
-void print(int *array[], int rows, int columns);
-
 void random_cells(int *array[], int rows, int columns);
--- step-2/game/game.c
+++ step-2/game/game.c
@@ -15,21 +15,6 @@ void create(int *array[], int rows, int columns)
   }
 }
 
-void print(int *array[], int rows, int columns)
-{
-  for (int i = 0; i < rows; i++)
-  {
-    for (int j = 0; j < columns; j++)
-    {
-      if (array[i][j] == 0)
-        printf(BRED " %d ", array[i][j]);
-      else
-        printf(BBLU " %d ", array[i][j]);
-    }
-    printf(reset "\n");
-  }
-}
-
 void random_cells(int *array[], int rows, int columns)
 {
   for (int i = 0; i < rows; i++)
